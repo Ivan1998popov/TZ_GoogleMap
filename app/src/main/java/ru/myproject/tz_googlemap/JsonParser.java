9c271d5475bf0ad7029c94dd312e62ff2e199902
Ivan <sportsmen172016@mail.ru>
2019-09-17 19:24:01 +0300

    finish 2.0

--- app/src/main/java/ru/myproject/tz_googlemap/JsonParser.java
+++ app/src/main/java/ru/myproject/tz_googlemap/JsonParser.java
@@ -4,6 +4,9 @@ import android.annotation.SuppressLint;
 import android.os.AsyncTask;
 
 import com.google.maps.android.clustering.ClusterManager;
+import com.google.maps.android.clustering.algo.GridBasedAlgorithm;
+import com.google.maps.android.clustering.algo.NonHierarchicalDistanceBasedAlgorithm;
+import com.google.maps.android.clustering.algo.PreCachingAlgorithmDecorator;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -15,6 +18,7 @@ public class JsonParser extends AsyncTask<Void, Void, List<Coordinates>> {
 
     private ClusterManager<MyItem> mClusterManager;
     private List<Coordinates> mCoordinates = new ArrayList<>();
+    private  MyItem offsetItem;
 
     @SuppressLint("StaticFieldLeak")
     private MapsActivity mapsActivity;
@@ -37,32 +41,39 @@ public class JsonParser extends AsyncTask<Void, Void, List<Coordinates>> {
                 mCoordinates.add(new Coordinates(innerArray.getString(0),
                         innerArray.getString(1), innerArray.getString(2)));
             }
-            //  System.out.println("/////// " + mCoordinates.size());
+            System.out.println("/////// " + mCoordinates.size());
         } catch (JSONException e) {
             e.printStackTrace();
         }
+
         return mCoordinates;
     }
 
 
     @Override
     protected void onPostExecute(List<Coordinates> coordinates) {
+
         setUpClusterer();
         super.onPostExecute(coordinates);
     }
 
 
     private void setUpClusterer() {
+
         mClusterManager = new ClusterManager<>(mapsActivity, mapsActivity.mMap);
         mapsActivity.mMap.setOnCameraIdleListener(mClusterManager);
         mapsActivity.mMap.setOnMarkerClickListener(mClusterManager);
+        mClusterManager.setAlgorithm(new GridBasedAlgorithm<>());
         addItems();
+
     }
 
     private void addItems() {
-        for (Coordinates cor : mCoordinates) {
-            MyItem offsetItem = new MyItem(Double.parseDouble(cor.getLatitude()),
-                    Double.parseDouble(cor.getLongitude()));
+        for (int i = 0; i <mCoordinates.size() ; i++) {
+           offsetItem = new MyItem(Double.parseDouble(mCoordinates.get(i).getLongitude()),
+                    Double.parseDouble(mCoordinates.get(i).getLatitude()));
+
+
             mClusterManager.addItem(offsetItem);
         }
     }
--- app/src/main/java/ru/myproject/tz_googlemap/MapsActivity.java
+++ app/src/main/java/ru/myproject/tz_googlemap/MapsActivity.java
@@ -1,6 +1,10 @@
 package ru.myproject.tz_googlemap;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.os.AsyncTask;
 import android.os.Bundle;
+
 
 import androidx.fragment.app.FragmentActivity;
 
@@ -11,9 +15,13 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 
+
+
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     public GoogleMap mMap;
+    SupportMapFragment mapFragment;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,11 +29,13 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         setContentView(R.layout.activity_maps);
 
 
-        new JsonParser(this).execute();
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+
+       mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         assert mapFragment != null;
         mapFragment.getMapAsync(this);
+
+        new JsonParser(this).execute();
 
     }
 
@@ -36,6 +46,12 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
 
+
     }
+
+
+
+
+
 
 }
